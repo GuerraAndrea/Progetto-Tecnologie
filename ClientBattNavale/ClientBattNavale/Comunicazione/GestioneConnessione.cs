7decc1354038fd350953444ba820d542982e4364
GuerraAndrea <andreaguerra2004@gmail.com>
2023-01-06 17:26:30 +0100

    classe dati condivisi

--- ClientBattNavale/ClientBattNavale/Comunicazione/GestioneConnessione.cs
+++ ClientBattNavale/ClientBattNavale/Comunicazione/GestioneConnessione.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using ClientBattNavale.Logica;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ClientBattNavale.Comunicazione
 {
-    class GestioneConnessione
+    public class GestioneConnessione
     {
+        private StreamWriter sw;
+        private StreamReader sr;
+        private bool termina;
+        private Queue<Messaggio> DaInviare;
+        private Queue<Messaggio> DaElaborare;
+        private object synElabora, synInvia;
+        private object synTerm;
+
+        public bool Termina { get { lock (synTerm) { return termina; } } set { lock (synTerm) { termina = value; } } }
+        public GestioneConnessione(TcpClient c)
+        {
+            synInvia = new object();
+            synElabora = new object();
+            synTerm = new object();
+            sw = new StreamWriter(c.GetStream());
+            sr = new StreamReader(c.GetStream());
+            termina = false;
+            DaInviare = new Queue<Messaggio>();
+            DaElaborare = new Queue<Messaggio>();
+            Thread client = new Thread(GClient);
+            Thread Server = new Thread(GServer);
+            Thread Logica = new Thread(GLogica);
+            client.Start();
+            Server.Start();
+            Logica.Start();
+        }
+       
+        
+
+
     }
 }
--- ClientBattNavale/ClientBattNavale/Logica/DatiCondivisi.cs
+++ ClientBattNavale/ClientBattNavale/Logica/DatiCondivisi.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ClientBattNavale.Comunicazione;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,21 @@ namespace ClientBattNavale.Logica
 {
     class DatiCondivisi
     {
+        public static DatiCondivisi instance = null;
+        public GestioneConnessione avversario;
+        public MainWindow main;
+        public Mappa mappa;
+        private DatiCondivisi()
+        {
+            TcpClient c = new TcpClient("192.168.1.2", 12345);
+            avversario = new GestioneConnessione(c);
+            mappa = new Mappa();
+        }
+        public static DatiCondivisi Init()
+        {
+            if (instance == null)
+                instance = new DatiCondivisi();
+            return instance;
+        }
     }
 }
