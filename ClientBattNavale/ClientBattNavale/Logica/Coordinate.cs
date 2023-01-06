65b66c203f2b87fc8e29ca81a5dc1290c666f91e
GuerraAndrea <andreaguerra2004@gmail.com>
2023-01-06 18:12:11 +0100

    classe coordinate

--- ClientBattNavale/ClientBattNavale/Logica/Coordinate.cs
+++ ClientBattNavale/ClientBattNavale/Logica/Coordinate.cs
@@ -8,5 +8,26 @@ namespace ClientBattNavale.Logica
 {
     class Coordinate
     {
+        public char Colonna;
+        public int Riga;
+        public Coordinate(string csv)
+        {
+            string[] split = csv.Split(';');
+            Colonna = split[0][0];
+            Riga = int.Parse(split[1]);
+        }
+        public Coordinate(char colonna, int riga)
+        {
+            Colonna = colonna;
+            Riga = riga;
+        }
+        public int getNumeroColonna()
+        {
+            return Colonna - 'A';
+        }
+        public string toCSV()
+        {
+            return Riga + ";" + Colonna;
+        }
     }
 }
