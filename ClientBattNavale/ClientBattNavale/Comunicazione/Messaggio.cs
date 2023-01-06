3e7fd859a9a3d92b3d0251e320542bf61da8a632
GuerraAndrea <andreaguerra2004@gmail.com>
2023-01-06 17:34:59 +0100

    classe messaggio

--- ClientBattNavale/ClientBattNavale/Comunicazione/Messaggio.cs
+++ ClientBattNavale/ClientBattNavale/Comunicazione/Messaggio.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ClientBattNavale.Comunicazione
 {
-    class Messaggio
+    public class Messaggio
     {
+        public string scelta, dati;
+        public Messaggio(string scelta, string dati)
+
+        {
+            this.scelta = scelta;
+            this.dati = dati;
+        }
+        public Messaggio(string csv)
+        {
+            int separatore = csv.IndexOf(';');
+            this.scelta = csv.Substring(0, separatore);
+            this.dati = csv.Substring(separatore + 1, csv.Length - separatore - 1);
+        }
+        public string toCsv()
+        {
+            return scelta + ";" + dati;
+        }
     }
 }
