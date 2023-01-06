4087b32e18cf80d4ee8ed97081cd220aa615119d
GuerraAndrea <andreaguerra2004@gmail.com>
2023-01-06 19:33:10 +0100

    metodi mappa

--- ClientBattNavale/ClientBattNavale/Logica/Mappa.cs
+++ ClientBattNavale/ClientBattNavale/Logica/Mappa.cs
@@ -42,5 +42,34 @@ namespace ClientBattNavale.Logica
             DatiCondivisi.Init().avversario.Invia(new Messaggio("P", inizio.toCSV() + ";" + fine.toCSV()));
             return true;
         }
+        public void SubisciAttaccoNave(Coordinate attacco)
+        {
+            if (!turno)
+                mappa[attacco.Riga, attacco.getNumeroColonna()] = -1;
+        }
+        public void AttaccaNave(Coordinate attacco)
+        {
+            if (turno)
+            {
+                if (attacchi[attacco.Riga - 1, attacco.getNumeroColonna()] == 0)
+                {
+                    attacchi[attacco.Riga - 1, attacco.getNumeroColonna()] = -1;
+                    DatiCondivisi.Init().avversario.Invia(new Messaggio("A", attacco.toCSV()));
+                    DatiCondivisi.Init().avversario.Invia(new Messaggio("t", ""));
+                }
+            }
+        }
+
+        internal void NaveAbbattuta(Coordinate inizio, Coordinate fine)
+        {
+            if (inizio.Riga == fine.Riga)
+                for (int i = Math.Min(inizio.getNumeroColonna(), fine.getNumeroColonna()); i <= Math.Max(inizio.getNumeroColonna(), fine.getNumeroColonna()); i++)
+                    mappa[inizio.Riga - 1, i] = 1;
+            else if (inizio.Colonna == fine.Colonna)
+                for (int i = Math.Min(inizio.Riga, fine.Riga) - 1; i < Math.Max(inizio.Riga, fine.Riga); i++)
+                    mappa[i, inizio.getNumeroColonna()] = 1;
+        }
     }
 }
+    
+
