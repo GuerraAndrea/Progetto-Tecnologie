9ca32d64ea2dfd525d0db28dcb34a1b2ff6eb60a
GuerraAndrea <andreaguerra2004@gmail.com>
2023-01-06 17:51:10 +0100

    metodo posizione navi

--- ClientBattNavale/ClientBattNavale/Client.xaml.cs
+++ ClientBattNavale/ClientBattNavale/Client.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientBattNavale.Logica;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ClientBattNavale
@@ -19,9 +21,34 @@ namespace ClientBattNavale
     /// </summary>
     public partial class Client : Window
     {
+        public Button[,] buttons = new Button[10, 10], attacchi = new Button[10, 10];
+        static int[] navi = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+        int naviInserite;
         public Client()
         {
             InitializeComponent();
+            DatiCondivisi.Init().main = this;
         }
+        public void InitPosizioneNavi()
+        {
+
+            naviInserite = 0;
+            TipoNave.Content = "Inserire una nave di " + navi[naviInserite] + " celle";
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button b = new Button();
+                    b.Width = InitGrigliaNavi.Width / 10;
+                    b.Height = InitGrigliaNavi.Height / 10;
+                    b.Margin = new Thickness(j * InitGrigliaNavi.Width / 10, i * InitGrigliaNavi.Height / 10, (9 - j) * InitGrigliaNavi.Width / 10, (9 - i) * InitGrigliaNavi.Width / 10);
+                    b.Click += B_Click;
+                    b.Name = "B" + (i + 1) + "_" + j;
+                    buttons[i, j] = b;
+                    InitGrigliaNavi.Children.Add(b);
+                }
+            }
+        }
+       
     }
 }
