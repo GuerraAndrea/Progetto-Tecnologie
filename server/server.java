86079a5631a7bab8782706ad03def3aff6d7f624
SimoneC04 <118894553+SimoneC04@users.noreply.github.com>
2023-01-17 16:36:14 +0100

    modifiche server

--- server/server.java
+++ server/server.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class server {
 
     public static void main(String[] args) throws IOException {
-        serverThread server = new serverThread();
-        server.connection();
+        serverThread server1 = new serverThread();
+        server1.connection();
     }
 
 }
